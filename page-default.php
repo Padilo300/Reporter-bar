d590eedec80113cdd671aa6a97ca4c91c4666c14
padilo300 <padilo300@gmail.com>
2018-02-27 18:18:13 +0200

    Немного опаздал с коммитом)

--- page-default.php
+++
@@ -1,7 +0,0 @@
-<?php require_once 'head.php'; ?>
-<body>
-  <div class="container-fluid">
-    <?php require_once 'php/part-page/nav-menu-main.php'  ; ?>
-    <div class="row" id="ajax-conteiner">
-    	
-<?php require_once 'footer.php'; ?>
--- php/script/schedule_Day.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	require_once 'change-schedule.php';
-	$changeSchedule 	= 	new chengeSchedule;
- 	echo $changeSchedule->scheduleDay($_GET['month'],$_GET['year'],$_GET['bar'],$_GET['userNumber']);
-
- ?>
-
